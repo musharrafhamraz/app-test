614a3e9101a83286b642e53799990d5e3bd3720b
musharrafhamraz <musharrafhamraz02@gmail.com>
2024-07-01 14:31:14 +0500

    hello.

--- App.js
+++ App.js
@@ -1,8 +1,6 @@
 import * as React from 'react';
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
-import * as tf from "@tensorflow/tfjs";
-import { bundleResourceIO } from "@tensorflow/tfjs-react-native";
 import SplashScreen from './screens/SplashScreen';
 import MusicPlayer from './screens/hello_screen';
 import OfflineClassifierV2 from './screens/OfflinePredV2';
